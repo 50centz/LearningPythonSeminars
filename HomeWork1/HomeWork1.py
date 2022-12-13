09845ee207e2eeeb49f377b41901f056511b8927
Федоров Евгений <zen-g@mail.ru>
2022-12-13 14:02:59 +0300

    Solved fourth  task

--- HomeWork1/HomeWork1.py
+++ HomeWork1/HomeWork1.py
@@ -31,18 +31,36 @@
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
 
-x = float(input('Enter the coordinates of the X point: '))
-y = float(input('Enter the coordinates of the Y point: '))
+# x = float(input('Enter the coordinates of the X point: '))
+# y = float(input('Enter the coordinates of the Y point: '))
 
-if(x == 0 or y == 0):
-    print('The coordinates must not be zero !!!')
-elif(x > 0 and y > 0):
-    print('1 plane')
-elif(x < 0 and y > 0):
-    print('2 plane')
-elif(x < 0 and y < 0):
-    print('3 plane')
-elif(x > 0 and y < 0):
-    print('4 plane')                
+# if(x == 0 or y == 0):
+#     print('The coordinates must not be zero !!!')
+# elif(x > 0 and y > 0):
+#     print('1 plane')
+# elif(x < 0 and y > 0):
+#     print('2 plane')
+# elif(x < 0 and y < 0):
+#     print('3 plane')
+# elif(x > 0 and y < 0):
+#     print('4 plane')                
 
 
+######################################################################################################################################
+
+
+# 4. Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+number = int(input('Enter the plane number: '))
+
+if(number < 1 or number > 4):
+    print('Invalid number !!!')
+elif(number == 1):
+    print('Range of possible coordinates x > 0 and y > 0')
+elif(number == 2):
+    print('Range of possible coordinates x < 0 and y > 0')
+elif(number == 3):
+    print('Range of possible coordinates x < 0 and y < 0')
+elif(number == 4):
+    print('Range of possible coordinates x > 0 and y < 0')            
