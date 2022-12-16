02c46d35169a2bf124854646400ba4bbc9b4c18f
Федоров Евгений <zen-g@mail.ru>
2022-12-16 12:01:17 +0300

    Second seminar complete

---
+++ HomeWork2/HomeWork2.py
@@ -0,0 +1,66 @@
+import random
+
+# from random import randint
+# randint(a, b)
+# from random import randint as RI
+# Ri(a, b)
+
+
+# 1. Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+
+# Пример:
+
+# - 6782 -> 23
+# - 0,56 -> 11
+
+
+# number = float(input('Input a number: '))
+# number = str(number)
+# number = list(number)
+
+# my_list = []
+
+# for item in number:
+#     my_list.append(item.replace('.', '0'))
+
+# result = list(map(int, my_list))
+
+# total = 0
+
+# for i in result:
+#     total += i
+
+# print(total)
+
+# number = float(input('Input a number: '))
+
+# number = str(number)
+
+# sum = 0
+
+# for i in number:
+#     if i.isdigit():
+#         sum += int(i)
+
+# print(sum)        
+
+###########################################################################################################################
+
+# 2. Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+# Пример:
+
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+
+# num = 4
+
+# new_list = []
+
+
+
+
+
+
+
+# print(random.randint(0, 100))
