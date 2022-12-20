bf7ee822d7300e88506bc508e3c91fc7a7b8798c
Федоров Евгений <zen-g@mail.ru>
2022-12-20 09:41:57 +0300

    Solved second task

--- HomeWork3/HomeWork3.py
+++ HomeWork3/HomeWork3.py
@@ -8,28 +8,69 @@ import random
 
 
 
+# a = int(input('Enter the size of the list: '))
+
+# Заполняем список рандомными числами, размер списка вводит пользователь
+
+# my_list = []
+
+# for i in range(1, a + 1):
+#     my_list.append(random.randint(0, 100))
+
+# print(*my_list)
+
+# Суммируем элементы на нечётных позициях     
+
+# sum = 0
+
+# for i in my_list[1::2]:
+#     sum += i
+    
+# print(sum)
+
+
+
+
+####################################################################################################################################
+
+
+
+# 2. Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# Пример:
+
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
+
+
 a = int(input('Enter the size of the list: '))
 
 # Заполняем список рандомными числами, размер списка вводит пользователь
 
 my_list = []
 
-for i in range(1, a + 1):
-    my_list.append(random.randint(0, 100))
+for i in range(0, a):
+    my_list.append(random.randint(0, 10))
 
-print(*my_list)
+print(my_list)
 
-# Суммируем элементы на нечётных позициях     
+my_list2 = []
 
-sum = 0
+size = 0
+if len(my_list) % 2 == 0:
+    size = int(len(my_list) / 2)
+else:
+    size = int(len(my_list) / 2 + 1)
 
-for i in my_list[1::2]:
-    sum += i
-    print(i)
+print(size)        
+b = -1
+
+for i in range(0, size):
+    my_list2.append(my_list[i] * my_list[b])
+    b = -1 + b
     
-print(sum)
+
+print(my_list2)   
 
 
-
-
-####################################################################################################################################
