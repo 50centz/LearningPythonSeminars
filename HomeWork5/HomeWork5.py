a5491f0ed2c36459b91014e269b7b7af17bcdeb7
Федоров Евгений <zen-g@mail.ru>
2023-01-13 16:52:41 +0300

    Work to home

--- HomeWork5/HomeWork5.py
+++ HomeWork5/HomeWork5.py
@@ -6,40 +6,47 @@ import random
 # b) Подумайте как наделить бота 'интеллектом'
 
 
-candies = int(input('Enter the number of candies: '))
+# candies = int(input('Enter the number of candies: '))
 
-oneMove = 28
+# oneMove = 28
 
-victory = candies % (oneMove + 1)
+# victory = candies % (oneMove + 1)
 
-men = 1
-bot = 0
+# men = 1
+# bot = 0
 
-firstMove = random.randint(0, 1)
-print(firstMove)
+# firstMove = random.randint(0, 1)
+# print(firstMove)
 
-if firstMove == men:
-    candies -= victory
-    while candies > 0:
-        bot = random.randint(1, 28)
-        candies -= bot
-        if candies == 0:
-            print('Winner bot')
-        men = (oneMove + 1) - bot
-        candies -= men
-        if candies == 0:
-            print('Winner men')
-else:
-    while candies > 0:
-        bot = random.randint(1, 28)
-        candies -= bot
-        if candies <= 0:
-            print('Winner bot')
-            break
-        men = random.randint(1, 28)
-        candies -= men
-        if candies <= 0:
-            print('Winner man')
-            break
+# if firstMove == men:
+#     candies -= victory
+#     while candies > 0:
+#         bot = random.randint(1, 28)
+#         candies -= bot
+#         if candies == 0:
+#             print('Winner bot')
+#         men = (oneMove + 1) - bot
+#         candies -= men
+#         if candies == 0:
+#             print('Winner men')
+# else:
+#     while candies > 0:
+#         bot = random.randint(1, 28)
+#         candies -= bot
+#         if candies <= 0:
+#             print('Winner bot')
+#             break
+#         men = random.randint(1, 28)
+#         candies -= men
+#         if candies <= 0:
+#             print('Winner man')
+#             break
+
+
+#########################################################################################################################################################################
+
+
+# Создайте программу для игры в 'Крестики-нолики'
+# НЕОБЯЗАТЕЛЬНО Добавить игру против бота с интеллектом
 
 
